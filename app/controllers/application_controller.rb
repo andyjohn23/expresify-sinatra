1733a980f5ba2413771ce10b5ee15bea760d75e6
andyjohn23 <andrewnick906@gmail.com>
2022-10-12 22:22:06 +0300

    removed the users routes and model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,19 +15,6 @@ class ApplicationController < Sinatra::Base
     updated_posts.to_json
   end
 
-  get "/user/posts/:id" do
-      posts = Post.all.where(["user_id= ?", params[:id]]).order(updated_at: :desc)
-      updated_posts = posts.map do |post|
-        {
-          id: post.id,
-          title: post.title,
-          content:post.content
-        }
-      end
-      updated_posts.to_json
-    
-  end
-
   post "/posts" do
     post = Post.create(
       title:params[:title],
@@ -45,50 +32,5 @@ class ApplicationController < Sinatra::Base
           content: post.content
         }
       updated_post.to_json
-  end
-  
-  # User routes
-  post "users/signup" do
-    user = User.find_by(email:params[:email])
-
-    if user == nil
-      new_user = User.create(
-        username:params[:username],
-        email:params[:email],
-        password:params[:password]
-      )
-      new_user.to_json
-    else
-      status 409
-      { errors: "User already exist" }.to_json
-    end
-  end
-
-  post "users/login" do
-    user = User.find_by(email:params[:email])
-  
-    if user && user.authenticate(params[:password])
-      user_data = {
-          id: user.id,
-          email: user.email
-        }
-      user_data.to_json
-    else
-      status 403
-      { errors: "User doesn't exist" }.to_json
-    end
-  end
-
-  patch "/users/:id" do
-    user = User.find(params[:id])
-    user.update(
-      username:params[:username]
-      )
-    user_data = {
-      id: user.id,
-      username: user.username,
-      email: user.email
-    }
-  user_data.to_json
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,4 @@ ActiveRecord::Schema.define(version: 2022_10_12_182136) do
     t.string "title"
     t.string "content"
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
